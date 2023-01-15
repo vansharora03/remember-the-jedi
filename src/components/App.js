bf8ded8d0dfbf21ce7f705206394d08233309175
vansharora03 <vansh1993@icloud.com>
2023-01-14 19:25:22 -0500

    Set up the state of App.js
    -Stores 3 things
     -Score
     -Best Score
     -Array of cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  //state to store the current score
+  const [score, setScore] = useState(0)
+
+  //state to store the best score
+  const [bestScore, setBestScore] = useState(0)
+
+  //state to store the array of cards
+  const [cards, setCards] = useState([
+    {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+     {name: "",
+     imgSrc : "",
+     description: ""},
+  ])
+
   return (
     <div className="App">
-      Hello world
+      <div className="score" id="score">Score : {score}</div>
+      <div className="score" id="best-score">Best Score : {bestScore}</div>
     </div>
   );
 }
