38506bab1774ec76b17e8320bcb40ef01fbfca6b
vansharora03 <vansh1993@icloud.com>
2023-01-14 20:24:16 -0500

    Created CardView and what it renders
    -Renders the list of cards from parent App
    -Uses that data to populate an array of React elements

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 export default function CardView(props) {
+    //destructure props
+    const [cards, randomizeCards, incrementScore, resetScore] = props;
+
+    //map the cards props to renderable JSX elements
+    const cardsToJSX = cards.map(card => {
+        return (<div onClick={() => {
+            //if card was already
+            //picked, reset the score
+            if(card.picked) {
+                resetScore()
+            } else {
+                //if card was not picked,
+                //incremenet score
+                incrementScore(card)
+            }
+            //regardless, randomize the cards
+            randomizeCards()
+        }} className="card" key={`card-${card.imgSrc}`}>
+            <img className="card-image" key={`img-${card.imgSrc}`} alt={card.name}></img>
+            <div className="card-name" key={`card-${card.name}`}>{card.name}</div>
+            <div className="card-description" key={`card-${card.description}`}>{card.description}</div>
+        </div>)
+    })
     return (
-        <div>CardView</div>
+        <div className="CardView">
+            {cardsToJSX}
+        </div>
     )
 }
